e705bbc2848f17a6007eac004c572852ec8155cc
sarnox <sarnox@gmail.com>
2017-12-12 02:56:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,29 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+var narratorstr = [
+    'Our HotS team, once so well regarded, is now barely whispered aloud by decent folk.',
+    'I remember days when the team lived, and laughter could be heard from the teamspeak.',
+    'Our HotS team, once so well regarded, is now barely whispered aloud by decent folk.',
+    'Women and men; bronzies and silvers; fools and flamers. All will find their way to us now that the queue is clear.',
+    'The cobwebs have been dusted, the channels set straight. The teamspeak calls for its team, but there is no answer.',
+    'Once, our HotS team was the envy of team league...',
+    'I was lord of this game once, before the shitters and rejects made it their domain.',
+    'In time, you will know the tragic extent of my HotS team...',
+    'An eternity of futile queue times -- a penance for my unspeakable retirement.',
+    'Curiosity, interest and overwatch heroes -- mile markers on my road to damnation.',
+    "In truth I cannot tell how much time has passed since I've been queuing for team league.",
+    'I can still see their angry faces as they stormed the skype group, but the team was dead before they found me, and the meems were on their way.',
+    'Our HotS team, once so well regarded, is now barely whispered aloud by decent folk.'
+    ];
+
+function rng (low, high) {
+    return Math.random() * (high - low) + low;
+};
+
 client.on('message', message => {
     if (message.content === '!team') {
-        message.reply(':ancestor01: Our HotS team, once so well regarded, is now barely whispered aloud by decent folk.', {tts:true});
+        client.sendMessage(message, "Hello!", {tts:true}); 
     }
 });
 
